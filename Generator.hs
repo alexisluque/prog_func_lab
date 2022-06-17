7196c3bbc6f0504e2105665703628eb6e8618e52
Alexis Luque <aleluque99@gmail.com>
2022-06-17 17:02:57 -0300

    Fix Generator.hs

--- Generator.hs
+++ Generator.hs
@@ -12,12 +12,13 @@ generate :: Program -> Code
 generate (Program body) = genMainBody body
 
 genMainBody :: MainBody -> Code
-genMainBody []                         = [SKIP]
-genMainBody ((Decl varDef):statements) = genMainBody statements
-genMainBody ((Com stmt):statements)    = genStmt stmt ++ genMainBody statements
+genMainBody []                      = [SKIP]
+genMainBody ((Decl _):statements)   = genMainBody statements
+genMainBody ((Com stmt):statements) = assign stmt ++ genMainBody statements
 
-genVarDef :: VarDef -> Instr
-genVarDef (VarDef _ name) = SKIP
+assign :: Stmt -> Code
+assign (StmtExpr (Assign name e)) = genExpr e ++ [STORE name]
+assign stmt                       = genStmt stmt
 
 genStmt :: Stmt -> Code
 genStmt (StmtExpr e) = genExpr e
@@ -27,22 +28,24 @@ genStmt (If e b1 b2) = genExpr e
                           , JMPZ (length b1' + 2) -- JMPZ ELSE
                           ]
                        ++ b1'
-                       ++ [JUMP (length b2')] -- JUMP END
+                       ++ [ JUMP (length b2' + 1)] -- JUMP END
                        -- ELSE:
                        ++ b2'
-                      --  END
+                       --  END
   where (b1', b2') = (genBody b1, genBody b2)
 genStmt (While e b)  = e'
-                       ++ [ -- COND:
-                          PUSH 0
+                       ++ [ PUSH 0 -- COND:
                           , CMP
                           , JMPZ (length b' + 2) -- JMPZ END
                           ]
                        ++ b'
-                       ++ [JMPZ (-length b' - length e' - 3)] -- JUMP COND
-                      -- END:
-  where (e',b') = (genBody b, genExpr e)
-genStmt (PutChar e)  = [PUSH (charLitToInt e), WRITE]
+                       ++ [JUMP (-(length e' + length b' + 3))] -- JUMP COND
+                       -- END:
+  where (e',b') = (genExpr e, genBody b)
+genStmt (PutChar e)  = if isCharLit e
+                       then [PUSH val, WRITE]
+                       else [LOAD name, WRITE]
+  where (val, name) = (charLitToInt e, getVarName e)
 
 genBody :: Body -> Code
 genBody = concatMap genStmt
@@ -56,15 +59,15 @@ genExpr GetChar              = [READ]
 genExpr (Unary Neg e)        = genExpr e ++ [NEG]
 -- NOT
 genExpr (Unary Not e)        = e'
-                              ++ [ PUSH 0
-                                 , CMP
-                                 , JMPZ 3 -- JMPZ TRUE
-                                 , PUSH 0
-                                 , JUMP 2 -- JUMP END
-                                 -- TRUE:
-                                 , PUSH 1
-                                 -- END:
-                                 ]
+                               ++ [ PUSH 0
+                                  , CMP
+                                  , JMPZ 3 -- JMPZ TRUE
+                                  , PUSH 0
+                                  , JUMP 2 -- JUMP END
+                                  -- TRUE:
+                                  , PUSH 1
+                                  -- END:
+                                  ]
   where e' = genExpr e
 -- PLUS
 genExpr (Binary Plus e1 e2)  = e2' ++ e1' ++ [ADD]
@@ -82,7 +85,13 @@ genExpr (Binary Div e1 e2)   = e2' ++ e1' ++ [DIV]
 genExpr (Binary Mod e1 e2)   = e2' ++ e1' ++ [MOD]
   where (e1',e2') = (genExpr e1, genExpr e2)
 -- EQU
-genExpr (Binary Equ e1 e2)   = e2' ++ e1' ++ [CMP]
+genExpr (Binary Equ e1 e2)   = e2' ++ e1'
+                               ++ [ CMP
+                                  , JMPZ 3
+                                  , PUSH 0
+                                  , JUMP 2
+                                  , PUSH 1
+                                  ]
   where (e1',e2') = (genExpr e1, genExpr e2)
 -- LESS
 genExpr (Binary Less e1 e2)  = e2' ++ e1'
@@ -101,12 +110,13 @@ genExpr (Binary Less e1 e2)  = e2' ++ e1'
 genExpr (Binary And e1 e2)   = e2' ++ e1'
                                ++ [ PUSH 0
                                   , CMP
-                                  , JMPZ 6 --JUMP TRUE
+                                  , JMPZ 7 --JUMP TRUE
                                   , PUSH 0
                                   , CMP
-                                  , JMPZ 3 -- JUMP END
+                                  , JMPZ 4 -- JUMP END
                                   , PUSH 1
-                                  , JUMP 2 -- JUMP END
+                                  , JUMP 3 -- JUMP END
+                                  , JMPZ 1 -- POP y
                                   -- TRUE:
                                   , PUSH 0
                                   -- END:
@@ -119,16 +129,17 @@ genExpr (Binary Or e1 e2)    = e2' ++ e1'
                                   , JMPZ 6 --JUMP TRUE
                                   , PUSH 1
                                   , CMP
-                                  , JMPZ 3 -- JUMP END
+                                  , JMPZ 4 -- JUMP END
                                   , PUSH 0
-                                  , JUMP 2 -- JUMP END
+                                  , JUMP 3 -- JUMP END
+                                  , JMPZ 1 -- POP y
                                   -- TRUE:
                                   , PUSH 1
                                   -- END:
                                   ]
   where (e1',e2') = (genExpr e1, genExpr e2)
 -- ASSIGN
-genExpr (Assign name e)      = e' ++ [STORE name]
+genExpr (Assign name e)      = e' ++ [STORE name, LOAD name]
   where e' = genExpr e
 
 -- UTILS
@@ -136,7 +147,19 @@ genExpr (Assign name e)      = e' ++ [STORE name]
 
 charLitToInt :: Expr -> Integer
 charLitToInt (CharLit c) = toInteger $ ord c
-charLitToInt _           = error "getCharLit: La expresión no es un carácter."
+charLitToInt _           = error "charLitToInt: La expresión no es de tipo CharLit."
+
+isCharLit :: Expr -> Bool
+isCharLit (CharLit c) = True
+isCharLit _           = False
+
+getVarName :: Expr -> String
+getVarName (Var name) = name
+getVarName _          = error "getVarName: La expresión no es de tipo Var."
+
+isAssign :: Stmt -> Bool
+isAssign (StmtExpr (Assign _ _)) = True
+isAssign _                       = False
 
 -- Función auxiliar para usar en el REPL
 getProgram :: Either a b -> b
